8c401f62bd7d50e7f13b65cc8c4e13f1f1d58006
bokismoki <borisbosnjak084@gmail.com>
2020-03-21 15:43:43 +0100

    Products @patch

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -16,11 +16,32 @@ export class ProductsService {
         return [...this.products]
     }
 
-    getSingleProduct(prodId: string) {
-        const product = this.products.find(product => product.id === prodId)
-        if(!product) {
+    private findProduct(id: string): [Product, number] {
+        const productIndex = this.products.findIndex(product => product.id === id)
+        const product = this.products.find(product => product.id === id)
+        if (!product) {
             throw new NotFoundException('Could not find product')
         }
+        return [product, productIndex]
+    }
+
+    getSingleProduct(prodId: string) {
+        const product = this.findProduct(prodId)[0]
         return { ...product }
+    }
+
+    updateProduct(prodId: string, title: string, description: string, price: number) {
+        const [product, index] = this.findProduct(prodId)
+        const updatedProduct = { ...product }
+        if (title) {
+            updatedProduct.title = title
+        }
+        if (description) {
+            updatedProduct.description = description
+        }
+        if (price) {
+            updatedProduct.price = price
+        }
+        this.products[index] = updatedProduct
     }
 }
